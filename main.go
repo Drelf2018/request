5820d58317a3552e358997dad572c76b47434ab0
Drelf2018 <drelf2018@outlook.com>
2024-03-11 23:53:48 +0800

    fix: 我是煞笔

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package request
 
-import (
-	"net/http"
-)
-
 func Get(url string, opts ...Option) *Response {
 	return NewGet(url, opts...).Do()
 }
 
 func Post(url string, opts ...Option) *Response {
-	return NewPost(http.MethodPost, opts...).Do()
+	return NewPost(url, opts...).Do()
 }
 
 func Json[T any](job *Job) (out T) {
